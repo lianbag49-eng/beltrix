384553910c5cca8dda6fd3de56042c3471f78830
lianbag49-eng <lianbag49@gmail.com>
2026-09-15 19:08:14 +0900

    Isolate mocked browser traffic from service-worker network interception

--- web/playwright.config.cjs
+++ web/playwright.config.cjs
@@ -1,1 +1,2 @@
-module.exports={testDir:'./tests',testMatch:'**/*.spec.cjs',timeout:45000,workers:2,use:{baseURL:'http://127.0.0.1:8080',browserName:'chromium',trace:'retain-on-failure'},webServer:{command:'python3 -m http.server 8080',url:'http://127.0.0.1:8080',cwd:'..'},reporter:'list'};
+module.exports={testDir:'./tests',testMatch:'**/*.spec.cjs',timeout:45000,workers:2,use:{serviceWorkers:'block',baseURL:'http://127.0.0.1:8080',browserName:'chromium',trace:'retain-on-failure'},webServer:{command:'python3 -m http.server 8080',url:'http://127.0.0.1:8080',cwd:'..'},reporter:'list'};
+
